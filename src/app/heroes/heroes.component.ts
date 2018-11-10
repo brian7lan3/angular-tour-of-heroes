63cb99ca4ffe5c71612349865278a55a3796de2c
BoLan <brian7lan3@gmail.com>
2018-11-10 18:57:58 +0800

    找到 getHeroes 方法，并且把它替换为如下代码

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,10 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
+
   heroes: Hero[];
   selectedHero: Hero;
 
@@ -27,6 +24,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
