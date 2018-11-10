a459b2d35ea12cf3d63ec32b1e541b82f5ef060a
BoLan <brian7lan3@gmail.com>
2018-11-10 18:50:38 +0800

    删除 HEROES 的导入语句，因为你以后不会再用它了。 转而导入 HeroService。

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +12,7 @@ export class HeroesComponent implements OnInit {
   //   id: 1,
   //   name: 'Windstorm'
   // };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   constructor() { }
